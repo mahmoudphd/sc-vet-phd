7e00ba5544e54125873f7aa2d6b06d099be01f55
mahmoudphd <mahmoud.eldeeb.phd@gmail.com>
2025-07-22 01:18:19 +0300

    Update simulatedIoTData.ts

--- src/pages/simulatedIoTData.ts
+++ src/pages/simulatedIoTData.ts
@@ -1,7 +1,1 @@
-// src/pages/simulatedIoTData.ts
-
-export const simulatedIoTData = [
-  { category: 'Electricity', consumption: 150, co2: 100 },
-  { category: 'Diesel', consumption: 50, co2: 130 },
-  { category: 'Gas', consumption: 80, co2: 60 },
-];
+remove file content
