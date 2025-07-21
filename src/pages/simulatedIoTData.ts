5c92d708ba583e62d7976efe8f585954c3079520
mahmoudphd <mahmoud.eldeeb.phd@gmail.com>
2025-07-22 01:01:54 +0300

    Create simulatedIoTData.ts

---
+++ src/pages/simulatedIoTData.ts
@@ -0,0 +1,6 @@
+src/pages/CO2Footprint.tsx(31,22): error TS2552: Cannot find name 'simulatedIoTData'. Did you mean 'simulatedIoTCostData'?
+src/pages/CO2Footprint.tsx(31,44): error TS7006: Parameter 'item' implicitly has an 'any' type.
+error Command failed with exit code 2.
+info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
+Error: Command "yarn run build" exited with 2
+Exiting build container
