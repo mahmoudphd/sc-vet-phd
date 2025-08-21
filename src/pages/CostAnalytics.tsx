719b23b805bd6cba1343bad3dda4315563a89b7f
mahmoudphd <mahmoud.eldeeb.phd@gmail.com>
2025-08-21 03:14:48 +0300

    Update CostAnalytics.tsx

--- src/pages/CostAnalytics.tsx
+++ src/pages/CostAnalytics.tsx
@@ -31,7 +31,7 @@ import {
   CartesianGrid
 } from 'recharts';
 import { DownloadIcon, UploadIcon } from '@radix-ui/react-icons';
-
+}
 interface Item {
   name: string;
   qty?: number;
@@ -53,8 +53,9 @@ interface Item {
   originalUnitPrice?: number;
   originalHours?: number;
   originalHourlyRate?: number;
+  hours?: number; // Added missing property
+  hourlyRate?: number; // Added missing property
 }
-
 interface CostTotals {
   actual: number;
   budget: number;
